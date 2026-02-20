4d05328001d39b645cadd3401ba383e5809a9f55
patranoiu-ship-it <patranoiu.alexandrugabriel.studente@itispaleocapa.it>
2026-02-20 10:34:23 +0100

    Add files via upload
    secondo

--- supermercato_es_27.cpp
+++ supermercato_es_27.cpp
@@ -8,6 +8,32 @@ struct prodotto
 	string categoria;
 };
 
+void aggiungi(prodotto p[], int dim)
+{
+	
+}
+
+int visualizza(prodotto prod[], int dim)
+{
+	
+}
+
+int cerca(string p, string prod[], int dim)
+{
+	
+}
+
+bool cancella(prototto p, string prod[], int dim)
+{
+	
+}
+
+bool modifica(prototto p, string prod[], int dim)
+{
+	
+}
+
+
 int main()
 {
 	prodotto p;
