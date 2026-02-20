1fa4bd85599609564e02c35cbedc1d2cdbd9b76c
patranoiu-ship-it <patranoiu.alexandrugabriel.studente@itispaleocapa.it>
2026-02-20 10:25:21 +0100

    Add files via upload
    primo v

---
+++ supermercato_es_27.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct prodotto
+{
+	string mome_prod;
+	float prezzo;
+	string categoria;
+};
+
+int main()
+{
+	prodotto p;
+	prodotto supermercato[];
+}
